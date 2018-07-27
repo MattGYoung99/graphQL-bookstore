bf782fd7b3d39f1042b56bdf4c0d259d9076462c
Matt Young <mattgyoung99@gmail.com>
2018-07-27 14:35:34 -0500

    deleted files and added root booklist component

---
+++ client/src/components/BookList.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+
+class BookList extends Component {
+    render() {
+        return(
+            <ul id="book-list">
+                <li>Book Name</li>
+            </ul>
+        )
+    }
+}
+
+export default BookList;
